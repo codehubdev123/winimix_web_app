11f7e24ea0eac417b2051450f23d8f45deaae458
Admin <admin@admin.com>
2025-09-13 12:07:44 +0300

    Add featured store slider in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import AnimationWrapper from "@/components/animations/AnimationWrapper";
 import NewBanner from "@/components/newdesign/NewBanner";
 import NewCategories from "@/components/newdesign/NewCategories";
 import NewProductSlider from "@/components/newdesign/NewProductSlider";
+import NewStoresSlider from "@/components/newdesign/NewStoresSlider";
 
 const HomePage = () => {
   return (
@@ -10,6 +11,9 @@ const HomePage = () => {
       <NewCategories />
       <AnimationWrapper direction="left" delay={1}>
         <NewProductSlider />
+      </AnimationWrapper>
+      <AnimationWrapper direction="right" delay={2}>
+        <NewStoresSlider />
       </AnimationWrapper>
     </>
   );
